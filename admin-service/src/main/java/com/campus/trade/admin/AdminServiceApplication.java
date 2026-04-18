18dc23ff07092bff5f7920d5a0467c94e313bae2
jackerman99 <caidanni99@gmail.com>
2026-04-18 17:18:57 +0800

    集成NACOS并注册服务

--- admin-service/src/main/java/com/campus/trade/admin/AdminServiceApplication.java
+++ admin-service/src/main/java/com/campus/trade/admin/AdminServiceApplication.java
@@ -2,8 +2,11 @@ package com.campus.trade.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AdminServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
--- user-service/src/main/java/com/campus/trade/user/UserServiceApplication.java
+++ user-service/src/main/java/com/campus/trade/user/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.campus.trade.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
